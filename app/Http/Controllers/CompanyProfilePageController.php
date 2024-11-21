69c7a8135928d1e01612de2ebe834d3ceb5c5242
Mukin008 <mkhlsn14@gmail.com>
2024-11-21 11:57:02 +0700

    updated

--- app/Http/Controllers/CompanyProfilePageController.php
+++ app/Http/Controllers/CompanyProfilePageController.php
@@ -15,7 +15,7 @@ class CompanyProfilePageController extends Controller
             'title' => 'Profil Perusahaan',
             'companyLogo' => Storage::url('img/companyLogo.png'),
             'aboutImg' => Storage::url('img/ptldnmockup.jpg'),
-            'structural' => Storage::url('img/structural.jpg'),
+            'structural' => Storage::url('assets/Struktur_Organisasi_2.svg'),
             'structuralRotate' => Storage::url('img/structuralRotate.jpg'),
         ]);
     }
